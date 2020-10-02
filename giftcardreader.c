ce46c5436e57d7e3c7d7a61a454cf4e5acd9d0bc
Ridhima97 <ra3108@nyu.edu>
2020-10-02 18:58:26 -0400

    Added Part 3

--- giftcardreader.c
+++ giftcardreader.c
@@ -32,9 +32,11 @@ void animate(char *msg, unsigned char *program) {
                 regs[arg1] = *mptr;
                 break;
             case 0x02:
+                if(arg1>16) break;
                 *mptr = regs[arg1];
                 break;
             case 0x03:
+                if(arg1>16) break;
                 mptr += (char)arg1;
                 break;
             case 0x04:
@@ -42,6 +44,7 @@ void animate(char *msg, unsigned char *program) {
                 regs[arg2] = arg1;
                 break;
             case 0x05:
+                if(arg2>16 || arg1>16) break;
                 regs[arg1] ^= regs[arg2];
                 zf = !regs[arg1];
                 break;
@@ -188,6 +191,7 @@ struct this_gift_card *gift_card_reader(FILE *input_fd) {
 		/* JAC: Why aren't return types checked? */
 		fread(&ret_val->num_bytes, 4,1, input_fd);
         if(ret_val->num_bytes < 0) {
+            printf("Invalid value, exiting program!\n");
             exit(0);
         } 
 		// Make something the size of the rest and read it in
